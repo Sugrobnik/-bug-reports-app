21ca296b0baa58131d8b0fca85bcbac952a7915e
Sugrobnik <dk.itnl@gmail.com>
2019-12-09 19:34:28 +0300

    add exception

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
 declare(strict_types = 1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/Src/Exception/exception.php';
+
+header('Content-Type: text/plain', true);
+
+
+$db = new mysqli('dsadsad', 'root', '3101', 'blog');
+exit;
+
+$config = \App\Helpers\Config::getFileContent('fsfsda');
+var_dump($config);
+
+$application = new \App\Helpers\App();
+echo $application->getServerTime()->format('Y-m-d H:i:s'). PHP_EOL;
+echo $application->getLogPath(). PHP_EOL;
+echo $application->getEnvironment(). PHP_EOL;
+echo $application->isDebugMode(). PHP_EOL;
+if($application->isRunningFromConsole())
+{
+    echo 'from console';
+} else {
+    echo 'from browser';
+}
+
