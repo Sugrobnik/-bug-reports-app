33a72b6dad68cb54d8c56713d87f9526e00cb49c
Sugrobnik <dk.itnl@gmail.com>
2019-12-10 15:00:03 +0300

    add Logger

--- Src/Exception/MissingArgumentException.php
+++ Src/Exception/MissingArgumentException.php
@@ -2,7 +2,7 @@
 declare(strict_types = 1);
 namespace App\Exception;
 
-class MissingArguementException extends BaseException
+class MissingArgumentException extends BaseException
 {
     
 }
--- index.php
+++ index.php
@@ -6,22 +6,9 @@ require_once __DIR__ . '/Src/Exception/exception.php';
 
 header('Content-Type: text/plain', true);
 
-
-$db = new mysqli('dsadsad', 'root', '3101', 'blog');
-exit;
-
-$config = \App\Helpers\Config::getFileContent('fsfsda');
-var_dump($config);
-
-$application = new \App\Helpers\App();
-echo $application->getServerTime()->format('Y-m-d H:i:s'). PHP_EOL;
-echo $application->getLogPath(). PHP_EOL;
-echo $application->getEnvironment(). PHP_EOL;
-echo $application->isDebugMode(). PHP_EOL;
-if($application->isRunningFromConsole())
-{
-    echo 'from console';
-} else {
-    echo 'from browser';
-}
+$logger = new \App\Logger\Logger();
+$logger->log(
+    \App\Logger\LogLevel::EMERGENCY, 'There is an emergency', ['exception' => 'exception occuped']
+);
+$logger->info('User account created successfully', ['id' => 5]);
 
